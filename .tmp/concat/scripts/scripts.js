740026d9a81617319dd0ef1c27d2549160b73906
Brian Hooper <afellowhobbyist@gmail.com>
2015-12-22 16:48:40 -0500

    Latest Grunt Build and Firebase Deploy

--- .tmp/concat/scripts/scripts.js
+++ .tmp/concat/scripts/scripts.js
@@ -61,6 +61,23 @@ var app = angular.module('smokejumper', ['firebase','angular-md5','ui.router', '
           }]
         }
       })
+      .state('directory', {
+        url: '/directory',
+        controller: 'DirectoryCtrl as directoryCtrl',
+        templateUrl: 'directory/index.html',
+        resolve: {
+          auth: ["$state", "Users", "Auth", function($state, Users, Auth){
+            return Auth.$requireAuth().catch(function(){
+              $state.go('home');
+            });
+          }],
+          dashboard: ["Users", "Auth", function(Users, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              return Users.getProfile(auth.uid).$loaded();
+            });
+          }]
+        }
+      })
       .state('about', {
         url: '/about',
         templateUrl: 'static/about.html'
@@ -300,6 +317,52 @@ app.controller('DashboardCtrl', ["$state", "md5", "auth", function($state, md5, 
   }]);
 
 console.log('--> smokejumper/app/dashboard/dashboard.controller.js loaded');
+
+'use strict';
+app.controller('DirectoryCtrl', ["$state", "$scope", "FIREBASE_URL", "$firebaseObject", "$firebaseArray", "$stateParams", "ngTableParams", "$filter", "Users", function($state, $scope, FIREBASE_URL, $firebaseObject, $firebaseArray, $stateParams, ngTableParams, $filter, Users){
+    var usersRef = new Firebase(FIREBASE_URL+'users');
+    $scope.users = $firebaseArray(usersRef);
+
+    // getUser on /directory/user/view/:id route
+    // $scope.getProfile = function() {
+    //   var ref = new Firebase(FIREBASE_URL + 'users');
+    //   $scope.user = $firebaseObject(ref.child($stateParams.userId));
+    // };
+
+    // Since the data is asynchronous we'll need to use the $loaded promise.
+    // Once data is available we'll set the data variable and init the ngTable
+    $scope.users.$loaded().then(function(users) {
+      console.log(users.length); // data is loaded here
+      var data = users;
+
+      $scope.tableDirectory = new ngTableParams({
+            page: 1,            // show first page
+            count: 10,          // count per page
+            sorting: { title: 'asc' }    // initial sorting
+        }, {
+            total: data.length, // length of data
+            getData: function($defer, params) {
+                // use build-in angular filter
+                var orderedData = params.sorting() ? $filter('filter')(data, params.filter()) : data;
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+
+    });
+
+    // Listening for list updates to Procedures to update Table
+    var ref = new Firebase(FIREBASE_URL + 'users');
+    var list = $firebaseArray(ref);
+    list.$watch(function(event) {
+      console.log(event);
+      $scope.users.$loaded().then(function(){
+        $scope.tableDirectory.reload();
+      });
+    });
+
+}]);
+
+console.log('--> smokejumper/app/directory.controller.js loaded');
 
 'use strict';
 
